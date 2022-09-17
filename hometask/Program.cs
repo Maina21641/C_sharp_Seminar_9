5914daef69a4f80580a5da2563c081edd0bdf207
Alex <Yulien.ru@Yandex.ru>
2022-09-17 23:54:54 +0400

    added решения всех задач

---
+++ hometask/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace hometask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int SumLoop(int N)
+            {
+                int sum=0;
+                for (int i=1;i<=N;i++) sum+=i;
+                return sum;
+            }
+            int SumRec(int N)
+            {
+                if (N==0) return 0;
+                else return N + SumRec(N-1);
+            }
+            Console.WriteLine(SumLoop(2));
+            Console.WriteLine(SumRec(5));
+        }
+    }
+}
---
+++ task63/Program.cs
@@ -0,0 +1,13 @@
+﻿// напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+//1 вариант
+int n =5;
+for (int i = 1; i <= n; i++)
+{
+    System.Console.Write(i + ", ");
+}
+//2 вариант
+string showNumbers(int N)
+{
+    return (N == 1 ? "1" : showNumbers(N - 1) + N);
+}
+System.Console.Write(showNumbers(5));
